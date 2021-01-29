4d91d91de552338332bfd210d0b7d5cf6113812c
SkeletonKing20 <n.bonn2002@gmail.com>
2021-01-29 10:18:00 +0100

    More Bunny animations

--- Assets/Scripts/AnimationBunny.cs
+++ Assets/Scripts/AnimationBunny.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class AnimationBunny : MonoBehaviour
 {
     Animator animator;
-    private int paramIDHorizontal, paramIDVertical;
+    private int paramIDHorizontal, paramIDVertical, paramIDPush;
     void Awake()
     {
         animator = GetComponentInChildren<Animator>();
@@ -14,6 +14,7 @@ public class AnimationBunny : MonoBehaviour
     {
         paramIDHorizontal = Animator.StringToHash("horizontalMovement");
         paramIDVertical = Animator.StringToHash("verticalMovement");
+        paramIDPush = Animator.StringToHash("isMovingAnObject");
     }
 
     // Update is called once per frame
@@ -21,5 +22,13 @@ public class AnimationBunny : MonoBehaviour
     {
         animator.SetFloat(paramIDHorizontal, Input.GetAxisRaw("Horizontal"));
         animator.SetFloat(paramIDVertical, Input.GetAxisRaw("Vertical"));
+        if(Input.GetButtonDown("Jump"))
+        {
+            animator.SetBool(paramIDPush, true);
+        }
+        if (Input.GetButtonUp("Jump"))
+        {
+            animator.SetBool(paramIDPush, false);
+        }
     }
 }
