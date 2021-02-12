96e13a2cbfffd7962ea753c87d13612468b70fcc
SkeletonKing20 <n.bonn2002@gmail.com>
2021-02-12 14:08:21 +0100

    Mainmenu

--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -5,9 +5,48 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 public class StartGame : MonoBehaviour
 {
+    Vector3 initialPosition;
+    public Vector3 targetPosition;
+    bool start;
+    bool credits;
+    private void Start()
+    {
+        initialPosition = transform.position;
+        start = true;
+        credits = false;
+    }
+
+    private void Update()
+    {
+        switch (Input.GetAxisRaw("Vertical"))
+        {
+            case 1:
+                transform.position = initialPosition;
+                start = true;
+                credits = false;
+                break;
+            case -1:
+                transform.position = targetPosition;
+                credits = true;
+                start = false;
+                break;
+            default:
+                transform.position = transform.position;
+                break;
+        }
+
+        if (start && (Input.GetButtonDown("Jump")|| Input.GetKeyDown(KeyCode.Return)))
+        {
+            gameStart();
+        }
+        else if (credits && (Input.GetButtonDown("Jump") || Input.GetKeyDown(KeyCode.Return)))
+        {
+            creditsStart();
+        }
+    }
     public void gameStart()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(1);
     }
     public void creditsStart()
     {
